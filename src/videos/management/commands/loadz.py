c4ccbd47d842bd3c6103cca0c1c3c6aaafdda91e
andreburto <tuglyraisin@gmail.com>
2022-04-24 23:08:56 -0500

    Minor tweaks.

--- src/videos/management/commands/loadz.py
+++ src/videos/management/commands/loadz.py
@@ -1,7 +1,7 @@
 import os
 import glob
 
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from videos import models
 
--- src/videos/views.py
+++ src/videos/views.py
@@ -1,6 +1,4 @@
-from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.template.loader import render_to_string
 
 from . import models
